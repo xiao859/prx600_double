8477a42b71f6ca5854d22e641484904ee6620dbf
xiao859 <17309616906@163.com>
2025-08-22 17:37:41 +0800

    msg： 1. exp1和exp2调换

--- Src/main.c
+++ Src/main.c
@@ -331,9 +331,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 //                config_filament_ref_slop_debug(1);
         if (Is_CTMode())
         {
-            ctrl_data.expo[1]   = get_expo_pin(0);
+            ctrl_data.expo[0]   = get_expo_pin(0);
 
-            ctrl_data.expo[0]   = get_expo_pin(1);
+            ctrl_data.expo[1]   = get_expo_pin(1);
 
             ct_task();
         }
