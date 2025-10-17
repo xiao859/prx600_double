036b38433646fb1b794b9244145f946441965175
xiao859 <17309616906@163.com>
2025-10-17 16:22:16 +0800

    msg：JL上烧写程序

--- Src/main.c
+++ Src/main.c
@@ -155,7 +155,7 @@ int main(void)
     HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
 		//config_HVEn_signal(1);
 		//HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 1551);
-//  cali_data.para_save_flag =1;
+  //cali_data.para_save_flag =1;
     while (1)
     {
 
@@ -256,13 +256,13 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
         if (Is_CTMode())
         {
 					//测试B源连续模式及整机烧录
-//					  ctrl_data.expo[0]   = get_expo_pin(0);
+					  ctrl_data.expo[0]   = get_expo_pin(0);
 
-//            ctrl_data.expo[1]   = get_expo_pin(1);
-					//其余模式
-            ctrl_data.expo[1]   = get_expo_pin(0);
+            ctrl_data.expo[1]   = get_expo_pin(1);
+//					//其余模式
+//            ctrl_data.expo[1]   = get_expo_pin(0);
 
-            ctrl_data.expo[0]   = get_expo_pin(1);
+//            ctrl_data.expo[0]   = get_expo_pin(1);
 
             ct_task();
         }
